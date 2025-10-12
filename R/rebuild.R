77b7ff0d9a4968c33dd4c94fdb01c77a6c04caa1
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2025-10-12 08:51:34 +0100

    Added rebuild method for tibbles.

--- R/rebuild.R
+++ R/rebuild.R
@@ -1,4 +1,17 @@
 
+# Explicit method for tibble
+
+#' @exportS3Method cheapr::rebuild
+rebuild.tbl_df <- function(x, template, shallow_copy = TRUE, ...){
+  out <- NextMethod("rebuild")
+
+  # Make sure class is a plain tbl
+  cheapr::attrs_modify(
+    out, class = c("tbl_df", "tbl", "data.frame"),
+    .set = !shallow_copy
+  )
+}
+
 #' @exportS3Method cheapr::rebuild
 rebuild.grouped_df <- function(x, template, ...){
 
