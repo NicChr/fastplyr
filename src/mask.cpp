e917121cefbcd0e0bfbb32726bedff2085dba99a
Nick Christofides <118103879+NicChr@users.noreply.github.com>
2026-04-03 21:12:17 +0100

    Removed dead code

--- src/mask.cpp
+++ src/mask.cpp
@@ -49,20 +49,3 @@ SEXP new_bare_data_mask(){
   YIELD(4);
   return mask;
 }
-
-// void reset_mask_top_env(SEXP mask, int env_size){
-//   Rf_defineVar(top_env_sym, mask, R_NewEnv(R_EmptyEnv, false, env_size));
-// }
-//
-// void add_to_mask(SEXP mask, SEXP result, SEXP name){
-//
-//   SEXP top_env = get_mask_top_env(mask);
-//
-//   if (name != R_UnboundValue){
-//     Rf_defineVar(name, result, top_env);
-//   }
-//
-//   if (Rf_isNull(result)){
-//     R_removeVarFromFrame(name, top_env);
-//   }
-// }
